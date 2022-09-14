24f6f888f28e903567d1067232319c303304f7f3
KB <101477932+kburra193@users.noreply.github.com>
2022-09-13 19:04:37 -0500

    Update functionality.js

--- js/functionality/functionality.js
+++ js/functionality/functionality.js
@@ -1,10 +1,11 @@
 var $;
 $(document).ready(function () {
-  /**** JS logic for Menu Icon  *****/
+  /**** JS logic for Menu Icon and Filter Icon  *****/
   $(".hamburger .hamburger__inner").click(function () {
     $(".wrapper").toggleClass("active");
   })
   $("#filter-toggle, #close-filters").click(function () {
     $(".wrapper, #bs-canvas-right").toggleClass("active-filters");
+	$('body').css('display', 'flex');
   })
 })
